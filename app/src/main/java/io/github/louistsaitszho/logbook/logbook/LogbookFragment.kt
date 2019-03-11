17a68fb9ae10ef06a241ff450e993b9ac4d8b7e5
Louis Tsai <louis993546@gmail.com>
2019-03-11 22:15:00 +0100

    Use layout-generated bindings

--- app/src/main/java/io/github/louistsaitszho/logbook/logbook/LogbookFragment.kt
+++ app/src/main/java/io/github/louistsaitszho/logbook/logbook/LogbookFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import io.github.louistsaitszho.logbook.R
+import io.github.louistsaitszho.logbook.databinding.LogbookFragmentBinding
 
 class LogbookFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.logbook_fragment, container, true)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = LogbookFragmentBinding.inflate(inflater, container, true)
+        return binding.root
+    }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
--- app/src/main/java/io/github/louistsaitszho/logbook/page/PageFragment.kt
+++ app/src/main/java/io/github/louistsaitszho/logbook/page/PageFragment.kt
@@ -5,13 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import io.github.louistsaitszho.logbook.R
+import io.github.louistsaitszho.logbook.databinding.PageFragmentBinding
+import io.github.louistsaitszho.logbook.model.Page
 
 class PageFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.page_fragment, container, true)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = PageFragmentBinding.inflate(inflater, container, true)
+        //TODO use the binding to set viewModel
+        return binding.root
+    }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
     }
+
+    companion object {
+        fun <T : Page> newInstance(page: T) = PageFragment()
+    }
+
 }
