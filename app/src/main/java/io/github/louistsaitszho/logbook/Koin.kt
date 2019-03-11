ade8972a33373cba902da482c766cc965fa6ec82
Louis Tsai <louis993546@gmail.com>
2019-03-11 22:39:00 +0100

    Teach koin what WorkoutViewModel is

--- app/src/main/java/io/github/louistsaitszho/logbook/Koin.kt
+++ app/src/main/java/io/github/louistsaitszho/logbook/Koin.kt
@@ -2,10 +2,12 @@ package io.github.louistsaitszho.logbook
 
 import io.github.louistsaitszho.logbook.logbook.LogbookViewModel
 import io.github.louistsaitszho.logbook.page.PageViewModel
+import io.github.louistsaitszho.logbook.workout.WorkoutViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     viewModel { PageViewModel() }
     viewModel { LogbookViewModel() }
+    viewModel { WorkoutViewModel() }
 }
